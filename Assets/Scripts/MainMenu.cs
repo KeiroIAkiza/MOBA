bb9c0c143c693a7d365aca82c80c9d731de1227f
KeiroIAkiza <44756050+KeiroIAkiza@users.noreply.github.com>
2018-11-10 19:47:01 -0500

    Working progress

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,9 +7,8 @@ public class MainMenu : MonoBehaviour {
 
     public void PlayGame()
     {
-        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        PhotonNetwork.ConnectUsingSettings("V1.0a");
-        Debug.Log("Connected");
+        PhotonNetwork.ConnectUsingSettings("V1.0a");//Lines to be moved
+        Debug.Log("Connected");                     //to login scene
         PhotonNetwork.JoinLobby();
         Debug.Log("In Lobby");
         SceneManager.LoadScene("Lobby");
---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour {
+
+    public AudioMixer audioMixer;
+    public Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " X " + resolutions[i].height;
+            options.Add(option);
+        }
+        resolutionDropdown.AddOptions(options);
+    }
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("MasterVolume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+}
